22e930a3880e8dc7baa9f743fdcd6a9950b38709
Grzegorz Korzeniowski <korzeniowski.g@gmail.com>
2020-03-17 13:31:07 +0100

    Introduce MainComponent

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../App.css';
+
+class MainComponent extends Component {
+  render() {
+    return (
+      <div className="App-intro">
+        placeholder
+      </div>
+    );
+  }
+}
+
+export default MainComponent;
